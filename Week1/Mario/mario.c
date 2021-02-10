ea3bafa3c13b3bf74ccddb000fe1bf8629914270
yoko <s1A121700885@toyo.jp>
2021-02-10 17:35:47 +0900

    feat:task1(mario)

--- Week1/Mario/mario.c
+++ Week1/Mario/mario.c
@@ -1,40 +1,53 @@
 #include<cs50.h>
 #include<stdio.h>
 
-
+ int height;
 void makeBlock(int n)
 {
-    if(n<8)
-    {
-        for (int i = 0; i ==n||i<n; i++)
-    {
-        printf("%s","#");
-    }
+    int makeHeight= height;
+    // ここでブロックを作成する→やりたいことは逆さまにする。例えばブロックの数が＃＃なら.を一つにする
+        if(n>0||n==0)
+        {
+        for(int g = n;g<makeHeight;g++)
+        {
+            // printf("%d", g);
+             printf(" ");
+        }
+        }
+    // ここで変数数分のループしたブロックを作成する
+        for (int i = 0; i ==n||i<n ; i++)
+        {
+            printf("#");
+        }
         printf("%s","\n");
-    }
 }
 
 int get_positive_int(void);
 
 int main(void)
-{
-    int i = get_positive_int();
-  for(int j=0;j<i;j++)
-    {
-      makeBlock(j);
-    }
 
+{
+    // 関数heightをここで呼び出す
+    int i = get_positive_int();
+    // 変数分だけルーブする
+    for(int j=0;j<i;j++)
+    {
+        makeBlock(j);
+    }
 }
 
 // Prompt user for positive integer
 int get_positive_int(void)
 {
-    int height;
+
     do
     {
         height = get_int("height: ");
     }
-    // もし高さより1の方が大きかったらもう一度ループする
-    while (height < 1);
+    // もし高さより1の方が大きいかつ8以上だったらもう一度ループする
+    while (height < 1 || height > 8);
     return height;
 }
+
+
+
