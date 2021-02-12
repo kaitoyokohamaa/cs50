7efe92475bc79b26fd0d7444c2273d844629f23a
yoko <s1A121700885@toyo.jp>
2021-02-12 09:57:05 +0900

    learn:uppercase

---
+++ Week2/uppercase.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include<stdio.h>
+#include<string.h>
+#include <ctype.h>
+int main(void)
+{
+    string s= get_string("Before: ");
+    printf("After: ");
+    for (int i = 0,n = strlen(s);i<n;i++)
+    {
+        if(islower(s[i]))
+        {
+            printf("%c", toupper(s[i]));
+        }
+    }
+    printf("\n");
+}
