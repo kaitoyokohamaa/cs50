682b04c930c97df49386bcbe66f45492756a43e9
yoko <s1A121700885@toyo.jp>
2021-02-14 10:00:56 +0900

    fix:課題cashの修正

--- Week1/Cash/cash.c
+++ Week1/Cash/cash.c
@@ -3,32 +3,32 @@
 #include<math.h>
 int main(void)
 {
-    float makeChange=get_float("makeChange: ");
+    float makeChange=get_float("Change owed: ");
     int cents = round(makeChange * 100);
     int change = 0;
     do{
-    if(makeChange>=25){
+    if(cents>=25){
         change= change+1;
-        makeChange=makeChange-25;
+        cents=cents-25;
     }
-     else if(makeChange>=10 && makeChange<=25 )
+     else if(cents>=10 && cents<=25 )
     {
         // 10セント以上25セント未満の場合は10セントのお釣りを返す。
         change= change+1;
-         makeChange=makeChange-10;
-    }else if(10>=makeChange&&5<=makeChange)
+         cents=cents-10;
+    }else if(10>=cents&&5<=cents)
     {
         // 10セント未満5セント以上の場合は5セントのお釣りを返す
         change= change+1;
-        makeChange=makeChange-5;
+        cents=cents-5;
     }
-    else if(5>=makeChange&&0<=makeChange)
+    else if(50>=cents&&0<=cents)
     {
         // 5セント未満1セント以上であれば0-5の間でペニーを返す
         change= change+1;
-        makeChange=makeChange-1;
+        cents=cents-1;
     }
         }
-    while (makeChange >0);
+    while (cents >0);
     printf(" %d \n",change);
 }
