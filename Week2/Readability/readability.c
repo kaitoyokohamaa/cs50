b4cbf7199ead82d51ecc2c903c68954e57e0fe2d
yoko <s1A121700885@toyo.jp>
2021-02-18 11:26:28 +0900

    fix:最終提出コード

--- Week2/Readability/readability.c
+++ Week2/Readability/readability.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include<math.h>
 void chackCounts(int n);
 int main(void)
 {
@@ -9,10 +10,10 @@ int main(void)
     //文字列をカウントする
 
     int score =0;
-    int word=0;
+    int word=1;
     int sentence=0;
-    double L=0;
-    double S=0;
+    float L=0;
+    float S=0;
     int index=0;
     for(int i = 0; i <= strlen(text); i++)
     {
@@ -21,23 +22,24 @@ int main(void)
         {
              score=score+1;
         }
-        else if(text[i] == ' ')
+        else if( isspace(text[i]) != 0 )
         {
              word=word+1;
-        }else if(text[i] == '.' ||text[i] == '!'||text[i] == '?'  )
+        }else if(text[i] == '.' ||text[i] == '!'||text[i] == '?' ||text[i] == ':' )
         {
             sentence=sentence+1;
         }
     }
-    L=((double)score/(double)word)*100;
-    S=((double)sentence/(double)word)*100;
-    index = 0.0588 * L - 0.296 * S - 15.8;
+    L=(double)score/(double)word*100;
+    S=(double)sentence/(double)word*100;
+    index = round(0.0588 * (double)L - 0.296 * (double)S - 15.8);
+
     if(index<0){
         printf("Before Grade 1\n");
     }else if(index>16){
         printf("Grade 16+\n");
     }else{
-    printf("Grade %d",index-1);
+    printf("Grade %d",index);
     printf("\n");
     }
 }
