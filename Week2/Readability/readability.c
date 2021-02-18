741edfe6437319d7e9d862c0f8e2e99f8e0b5581
yoko <s1A121700885@toyo.jp>
2021-02-18 09:16:34 +0900

    feat:redabilityの処理

--- Week2/Readability/readability.c
+++ Week2/Readability/readability.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 void chackCounts(int n);
@@ -8,6 +10,9 @@ int main(void)
     int score =0;
     int word=1;
     int sentence=0;
+    double L=0;
+    double S=0;
+    int index=0;
     for(int i = 0; i <= strlen(text); i++)
     {
         // カウントしないタイプチェック
@@ -23,9 +28,16 @@ int main(void)
             sentence=sentence+1;
         }
     }
-    printf("%d\n",score);
-    printf("%d\n",word);
-    printf("%d\n",sentence);
-    
-    return score;
+
+    L=((double)score/(double)word)*100;
+    S=((double)sentence/(double)word)*100;
+    // printf("%f",(0.0588 * L )- (0.296 * S) - 15.8);
+    index = 0.0588 * (double)L - 0.296 * (double)S - 15.8;
+    if(index<0){
+        printf("Before Grade 1");
+    }else if(index>16){
+        printf("Grade 16+");
+    }else{
+    printf("Grade %d",index);
+    }
 }
