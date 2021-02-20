ddd4dba951e3a5df7925b98828da7219769065f9
yoko <s1A121700885@toyo.jp>
2021-02-20 14:19:01 +0900

    feat:課題の微調節

--- Week2/Caesar/caesar.c
+++ Week2/Caesar/caesar.c
@@ -7,43 +7,35 @@ int main(int argc, string argv[])
 {
 
     char *ascii = argv[1];
-    if(atoi(argv[1]) &&argc==2 &&isdigit(ascii[0])&& !isalpha(ascii[0]))
+    if (atoi(argv[1]) && argc == 2 && isdigit(ascii[0]) && !isalpha(ascii[0]))
     {
-    string plainText = get_string("plaintext: ");
-    char calc [222];
-    char box[111];
-    for(int i = 0,n= strlen(plainText); i<n;i++ )
-    {
-        if(isalpha(plainText[i]))
+        string plainText = get_string("plaintext: ");
+        char calc [222];
+        int upperbox[111];
+        char lowerbox[111];
+        char lastNum[111];
+        for (int i = 0, n = strlen(plainText); i < n; i++)
         {
-            if(isupper(plainText[i]))
+            if (isalpha(plainText[i]))
             {
-                if((plainText[i])==90)
+                if (isupper(plainText[i]))
                 {
-                    // upperbox[i]=plainText[i]-65;
-                    calc[i]=box[i]+atoi(argv[1])%26 + 65;
+                    upperbox[i] = plainText[i] - 65;
+                    calc[i] = (upperbox[i]+atoi(argv[1])) % 26;
+                    lastNum[i] = calc[i]+65;
                 }
-                else
+                else if (islower(plainText[i]))
                 {
-                calc[i] =plainText[i]+atoi(argv[1]);
-                }
-            }
-            else if(islower(plainText[i]))
-            {
-                if((plainText[i])==122)
-                {
-                    // lowerbox[i]=plainText[i]-97;
-                    calc[i]=box[i]+atoi(argv[1])%26 + 97;
-                }
-                else
-                {
-                calc[i] =plainText[i]+atoi(argv[1]);
+                    lowerbox[i] = plainText[i] - 97;
+                    calc[i] = (lowerbox[i] + atoi(argv[1])) % 26;
+                    lastNum[i] = calc[i] + 97;
                 }
             }
         }
+        printf("ciphertext: %s", lastNum);
+        printf("\n");
     }
-       printf("ciphertext:%s",calc );
-    }else if(!atoi(argv[1]) || argc!=2 ||!isdigit(ascii[0]) )
+    else if (!atoi(argv[1]) || argc != 2 || !isdigit(ascii[0]))
     {
             printf("Usage: ./caesar key");
     }
