11b9cf4cef9b6005ddb12cfdc97268c990d7e5db
yoko <s1A121700885@toyo.jp>
2021-03-13 22:07:55 +0900

    add file

---
+++ Week3/pset3/plurality/plurality.c
@@ -0,0 +1,102 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    string candidateName;
+    bool user = false;
+    // TODO
+    int x = 10;
+
+    if (x == 10){
+       printf("x は10と等しい¥n");
+     }
+    // for (int i=0;i <candidate_count;i++)
+    // {
+    //   candidateName = candidates[i].name;
+    // }
+
+    candidateName="Alice";
+    printf("%s",name);
+    if (candidateName == name )
+    {
+        printf("入ってるよ");
+        user= true;
+    }
+    return user;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // TODO
+    printf("%c",candidates[1].votes);
+    return;
+}
+
