a75e3631880989c4db5b8103c4d5b185d8a2edd3
kaitoyokohama <s1A121700885@toyo.jp>
2021-05-29 18:06:04 +0900

    codegym scrabble課題

--- Week2/Scrabble/scrabble.c
+++ Week2/Scrabble/scrabble.c
@@ -49,3 +49,67 @@ int compute_score(string word)
     }
         return score;
 }
+
+
+// 
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
+
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+    int sum = 0;
+    for (int i = 0; strlen(word) > i; i++)
+    {
+        int calcint = word[i];
+        if (isalpha(word[i]))
+        {
+            if (islower(word[i]))
+            {
+                sum += POINTS[calcint - 97];
+            }
+            else if (isupper(word[i]))
+            {
+                sum += POINTS[calcint - 65];
+            }
+        }
+        else
+        {
+            sum += 0;
+        }
+    }
+    return sum;
+}
