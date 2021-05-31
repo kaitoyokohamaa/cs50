1408a7fad071884051e206f30df8a1b1baf0ad5b
kaitoyokohama <s1A121700885@toyo.jp>
2021-05-31 18:55:41 +0900

    codegym caesar

--- Week2/Caesar/caesar.c
+++ Week2/Caesar/caesar.c
@@ -40,3 +40,64 @@ int main(int argc, string argv[])
             printf("Usage: ./caesar key");
     }
 }
+// codegym
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int doencrypt(string text, int num);
+int main(int argc, string argv[])
+{
+
+    if (argc == 2)
+    {
+        int num = atoi(argv[1]);
+        string text = get_string("plaintext:  ");
+        doencrypt(text, num);
+    }
+    else if (argc != 2)
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+}
+
+int doencrypt(string text, int num)
+{
+    char encrypttext[strlen(text)];
+    for (int i = 0 ; strlen(text) > i; i++)
+    {
+        int changedtext = text[i] + num;
+        encrypttext[i] = changedtext;
+        if (isalpha(text[i]))
+        {
+            if (changedtext <= 122 && changedtext >= 97)
+            {
+                encrypttext[i] = changedtext;
+            }
+            else if (changedtext <= 90 && changedtext >= 65)
+            {
+                encrypttext[i] = changedtext;
+            }
+            else
+            {
+                if (isupper(text[i]))
+                {
+                    encrypttext[i] = (text[i] - 65 + num) % 26 + 65;
+                }
+                else if (islower(text[i]))
+                {
+                    encrypttext[i] = (text[i] - 97 + num) % 26 + 97;
+                }
+            }
+
+        }
+        else
+        {
+            encrypttext[i] = text[i];
+        }
+    }
+    printf("ciphertext: %s\n", encrypttext);
+    return 0;
+}
